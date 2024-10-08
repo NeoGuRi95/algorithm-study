a2d90531e2f10d75f54b9f876552380e1345079b
Park JeaHyun <86871368+NeoGuRi95@users.noreply.github.com>
2024-10-08 22:22:47 +0900

    [level 3] Title: 카드 짝 맞추기, Time: 400.65 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/72415. 카드 짝 맞추기/카드 짝 맞추기.py
+++ 프로그래머스/3/72415. 카드 짝 맞추기/카드 짝 맞추기.py
@@ -4,101 +4,77 @@ import pprint
 
 def solution(board, r, c):
     def calc_command_cnt(start_r, start_c, end_r, end_c):
-        # 행 이동 -> 열 이동 경우
-        cur_r, cur_c = start_r, start_c
-        command_cnt_1 = 0
-        # 행 이동
         temp_command_cnt = 0
-        while cur_r != end_r:
-            if cur_r < end_r:
-                cur_r += 1
-            elif cur_r > end_r:
-                cur_r -= 1
-            temp_command_cnt += 1
-            if _board[cur_r][cur_c] > 0 or cur_r == 0 or cur_r == 3:
-                # 카드를 만나거나 방향의 마지막 칸인 경우 ctrl 이동
+        # 행 이동 횟수
+        row_command_cnt = 0
+        while start_r != end_r:
+            if start_r > end_r:
+                # 위 이동
+                start_r -= 1
+            elif start_r < end_r:
+                # 아래 이동
+                start_r += 1
+            if _board[start_r][start_c] > 0:
+                row_command_cnt += 1
                 temp_command_cnt = 0
-                command_cnt_1 += 1
-        command_cnt_1 += temp_command_cnt
-        # 열 이동
+            else:
+                temp_command_cnt += 1
+        if row_command_cnt > 0:
+            row_command_cnt += temp_command_cnt
+        else:
+            row_command_cnt = 1
+        # 열 이동 횟수
+        col_command_cnt = 0
         temp_command_cnt = 0
-        while cur_c != end_c:
-            if cur_c < end_c:
-                cur_c += 1
-            elif cur_c > end_c:
-                cur_c -= 1
-            temp_command_cnt += 1
-            if _board[cur_r][cur_c] > 0 or cur_c == 0 or cur_c == 3:
+        while start_c != end_c:
+            if start_c > end_c:
+                # 왼쪽 이동
+                start_c -= 1
+            elif start_c < end_c:
+                # 오른쪽 이동
+                start_c += 1
+            if _board[start_r][start_c] > 0:
+                col_command_cnt = 1
                 temp_command_cnt = 0
-                command_cnt_1 += 1
-        command_cnt_1 += temp_command_cnt
-        #################################
-        # 열 이동 -> 행 이동 경우
-        cur_r, cur_c = start_r, start_c
-        command_cnt_2 = 0
-        # 열 이동
-        temp_command_cnt = 0 
-        while cur_c != end_c:
-            if cur_c < end_c:
-                cur_c += 1
-            elif cur_c > end_c:
-                cur_c -= 1
-            temp_command_cnt += 1
-            if _board[cur_r][cur_c] > 0 or cur_c == 0 or cur_c == 3:
-                temp_command_cnt = 0
-                command_cnt_2 += 1
-        command_cnt_2 += temp_command_cnt
-        # 행 이동
-        temp_command_cnt = 0
-        while cur_r != end_r:
-            if cur_r < end_r:
-                cur_r += 1
-            elif cur_r > end_r:
-                cur_r -= 1
-            temp_command_cnt += 1
-            if _board[cur_r][cur_c] > 0 or cur_r == 0 or cur_r == 3:
-                temp_command_cnt = 0
-                command_cnt_2 += 1
-        command_cnt_2 += temp_command_cnt
+            else:
+                temp_command_cnt += 1
+        if col_command_cnt > 0:
+            col_command_cnt += temp_command_cnt
+        else:
+            col_command_cnt = 1
         
-        return min(command_cnt_1, command_cnt_2)
+        return row_command_cnt + col_command_cnt
 
-    def bfs(select_order, depth, cur_pos, command_cnt, path):
+    def bfs(select_order, depth, cur_pos, command_cnt):
+        print('----------------')
+        print('depth: ', depth)
+        print('cur_pos: ', cur_pos)
+        print('command cnt: ', command_cnt)
         if depth == len(select_order):
-            # pprint.pprint(path)
-            # print('command_cnt:', command_cnt)
-            # print()
+            print('return command cnt: ', command_cnt)
             return command_cnt
         card_type = select_order[depth]
         pos1, pos2 = card_pos_dict[card_type][0], card_pos_dict[card_type][1]
+        print('pos1, pos2: ', pos1, pos2)
         # cur_pos -> pos1 조작 횟수
         command_cnt1 = calc_command_cnt(cur_pos[0], cur_pos[1], pos1[0], pos1[1])
         # cur_pos -> pos2 조작 횟수
         command_cnt2 = calc_command_cnt(cur_pos[0], cur_pos[1], pos2[0], pos2[1])
-        # pos1 -> pos2 조작 횟수
+        # pos1 -> pos2 조작 횟수(pos2 -> pos1 조작 횟수 동일)
         command_cnt3 = calc_command_cnt(pos1[0], pos1[1], pos2[0], pos2[1])
-        # pos2 -> pos1 조작 횟수
-        command_cnt4 = calc_command_cnt(pos2[0], pos2[1], pos1[0], pos1[1])
         # 선택한 카드 제거
         _board[pos1[0]][pos1[1]] = 0
         _board[pos2[0]][pos2[1]] = 0
         # cur_pos -> pos1 -> pos2
-        path.append((cur_pos, pos1, pos2))
-        # print('----------------')
-        # print(cur_pos, ' -> ', pos1, ': ', command_cnt1)
-        # print(pos1, ' -> ', pos2, ': ', command_cnt3)
-        # print('----------------')
-        total_command_cnt = bfs(select_order, depth + 1, pos2, command_cnt + command_cnt1 + command_cnt3 + 2, path)
-        path.pop()
+        print('cur_pos -> pos1: ', command_cnt1)
+        print('pos1 -> pos2: ', command_cnt3)
+        total_command_cnt = bfs(select_order, depth + 1, pos2, command_cnt + command_cnt1 + command_cnt3 + 2)
         # cur_pos -> pos2 -> pos1
-        path.append((cur_pos, pos2, pos1))
-        # print(cur_pos, ' -> ', pos2, ': ', command_cnt2)
-        # print(pos2, ' -> ', pos1, ': ', command_cnt3)
-        # print('----------------')
+        print('cur_pos -> pos2: ', command_cnt2)
+        print('pos2 -> pos1: ', command_cnt3)
         total_command_cnt = min(
             total_command_cnt, 
-            bfs(select_order, depth + 1, pos1, command_cnt + command_cnt2 + command_cnt4 + 2, path))
-        path.pop()
+            bfs(select_order, depth + 1, pos2, command_cnt + command_cnt2 + command_cnt3 + 2))
         # 선택한 카드 부활
         _board[pos1[0]][pos1[1]] = card_type
         _board[pos2[0]][pos2[1]] = card_type
@@ -119,9 +95,9 @@ def solution(board, r, c):
     card_type_list = list(card_pos_dict.keys())
     select_order_list = list(permutations(card_type_list, len(card_type_list)))
     for select_order in select_order_list:
-        # print('select_order: ', select_order)
-        answer = min(bfs(select_order, 0, (r, c), 0, []), answer)
-        # print('answer: ', answer)
-        # print('--------------------')
+        print('select_order: ', select_order)
+        answer = min(bfs(select_order, 0, (r, c), 0), answer)
+        print('answer: ', answer)
+        print('--------------------')
     
     return answer
