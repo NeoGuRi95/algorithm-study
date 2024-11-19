eccdb6df4c188e0e71607dc149381bdeed5bd39c
Park JeaHyun <86871368+NeoGuRi95@users.noreply.github.com>
2024-11-19 14:16:14 +0900

    [Silver I] Title: 볼 모으기, Time: 476 ms, Memory: 53112 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -10,36 +10,64 @@ public class Main {
         String ballInfo = br.readLine();
         Stack<Character> rStack = new Stack<>();
         Stack<Character> bStack = new Stack<>();
+        Queue<Character> rQueue = new LinkedList<>();
+        Queue<Character> bQueue = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             char ball = ballInfo.charAt(i);
             rStack.add(ball);
             bStack.add(ball);
+            rQueue.add(ball);
+            bQueue.add(ball);
         }
 
-        // Red count
-        int rCnt = 0;
+        // Red stack count
+        int rStackCnt = 0;
         boolean firstFlag = true;
         while (!rStack.isEmpty()) {
             char ball = rStack.pop();
            if (ball == 'B' && firstFlag) {
                firstFlag = false;
            } else if (ball == 'R' && !firstFlag) {
-               rCnt++;
+               rStackCnt++;
            }
         }
 
-        // Blue count
-        int bCnt = 0;
+        // Blue stack count
+        int bStackCnt = 0;
         firstFlag = true;
         while (!bStack.isEmpty()) {
             char ball = bStack.pop();
             if (ball == 'R' && firstFlag) {
                 firstFlag = false;
             } else if (ball == 'B' && !firstFlag) {
-                bCnt++;
+                bStackCnt++;
             }
         }
 
-        System.out.println(Math.min(rCnt, bCnt));
+        // Red Queue count
+        int rQueueCnt = 0;
+        firstFlag = true;
+        while (!rQueue.isEmpty()) {
+            char ball = rQueue.poll();
+            if (ball == 'B' && firstFlag) {
+                firstFlag = false;
+            } else if (ball == 'R' && !firstFlag) {
+                rQueueCnt++;
+            }
+        }
+
+        // Red Queue count
+        int bQueueCnt = 0;
+        firstFlag = true;
+        while (!bQueue.isEmpty()) {
+            char ball = bQueue.poll();
+            if (ball == 'R' && firstFlag) {
+                firstFlag = false;
+            } else if (ball == 'B' && !firstFlag) {
+                bQueueCnt++;
+            }
+        }
+
+        System.out.println(Math.min(Math.min(rStackCnt, bStackCnt), Math.min(rQueueCnt, bQueueCnt)));
     }
 }
