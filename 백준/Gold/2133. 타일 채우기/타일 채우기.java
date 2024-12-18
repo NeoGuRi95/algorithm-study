51cd9dd8e7ca29d1042cd466cb632b90f74f27c6
Park JeaHyun <86871368+NeoGuRi95@users.noreply.github.com>
2024-12-18 19:00:13 +0900

    [Gold IV] Title: 타일 채우기, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    if (n % 2 == 1) {
+      System.out.println(0);
+      return;
+    }
+
+    int[][] dp = new int[n + 1][3];
+
+    dp[0][0] = 1;
+    dp[1][1] = 2;
+    dp[2][0] = 3;
+    dp[2][2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+      if (i % 2 == 0) { // 짝수
+        dp[i][0] = dp[i - 2][2] + 3 * dp[i - 2][0];
+        dp[i][2] = dp[i - 1][1];
+      } else { // 홀수
+        dp[i][1] = 2 * dp[i - 1][0] + dp[i - 1][2];
+      }
+    }
+
+    System.out.println(dp[n][0]);
+
+    br.close();
+  }
+}
