ad8c3986c426dbd7cda1aebf20c47afad027e06e
Park JeaHyun <86871368+NeoGuRi95@users.noreply.github.com>
2024-12-29 22:39:26 +0900

    [level 3] Title: 징검다리 건너기, Time: 32.40 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] stones, int k) {
+        int l = Integer.MAX_VALUE;
+        int r = 0;
+        for (int i = 0; i < stones.length; i++) {
+            l = Math.min(l, stones[i]);
+            r = Math.max(r, stones[i]);
+        }
+        
+        while (l < r) {
+            int mid = (l + r) / 2;
+            int jumpSize = 0;
+            
+            for (int i = 0; i < stones.length; i++) {
+                if (stones[i] <= mid) {
+                    jumpSize++;
+                } else {
+                    jumpSize = 0;
+                }
+                
+                if (jumpSize == k) break;
+            }
+            // System.out.println("mid: " + mid + ", jumpSize: " + jumpSize);
+            if (jumpSize < k) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        
+        return l;
+    }
+}
